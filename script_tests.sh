c30aa3dbda5e0c78138b646aa5b9af2c16cfec28
Guillermo Osorio <gaompy@gmail.com>
2018-11-27 12:45:41 -0300

    NaiveBayes

--- script_tests.sh
+++ script_tests.sh
@@ -5,6 +5,8 @@
 #  cd qoe_datasets
 # Segundo, corroborrar que la variable de entorno CLASSPATH contenga a weka.jar. Ejemplo:
 #  export CLASSPATH=~/weka-3-8-3/weka.jar:$CLASSPATH
+# Normalmente se lleva a cabo CV (cross-validations) cuando no se especifica un archivo de Training (opción -T)
+# y se utiliza el parámetro -s para indicar una semilla para agrupar de forma aleatoria a los conjuntos de Training y Validation.
 
 CURR_PWD=$(pwd)
 
@@ -14,11 +16,17 @@ for i in `seq 1 7`; do
 		for DATASET in data data_filled_r data_filled_weka; do
 			mkdir -p $CURR_PWD/results/$i
 			OUTPUT_DIR=$CURR_PWD/results/$i
-			# RandomForest
+
+			# Clasificadores Bayesianos
+			## RandomForest
 			OUTPUT_FILE=$OUTPUT_DIR/RandomForest_$DATASET_$CV_SEED.txt
 			java weka.classifiers.trees.RandomForest -P 100 -I 100 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1 -s $CV_SEED -t $CURR_PWD/$i/$DATASET.arff > $OUTPUT_FILE
+			## Naive Bayes
+			OUTPUT_FILE=$OUTPUT_DIR/NaiveBayes_$DATASET_$CV_SEED.txt
+			java weka.classifiers.bayes.NaiveBayes -s $CV_SEED -t $CURR_PWD/$i/$DATASET.arff > $OUTPUT_FILE
 
-			# RandomTree
+			# Árboles de decisión
+			## RandomTree
 			OUTPUT_FILE=$OUTPUT_DIR/RandomTree_$DATASET_$CV_SEED.txt
 			java weka.classifiers.trees.RandomTree -K 0 -M 1.0 -V 0.001 -S 1 -s $CV_SEED -t $CURR_PWD/$i/$DATASET.arff > $OUTPUT_FILE
 
