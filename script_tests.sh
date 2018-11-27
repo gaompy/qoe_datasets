56f86ea1c4cd941f3b360e4086df78327c5fec69
Guillermo Osorio <gaompy@gmail.com>
2018-11-27 12:28:32 -0300

    RandomTree agregado

--- script_tests.sh
+++ script_tests.sh
@@ -3,15 +3,26 @@
 # Primero se debe clonar el repo y acceder al directorio
 #  git clone https://github.com/gaompy/qoe_datasets
 #  cd qoe_datasets
+# Segundo, corroborrar que la variable de entorno CLASSPATH contenga a weka.jar. Ejemplo:
+#  export CLASSPATH=~/weka-3-8-3/weka.jar:$CLASSPATH
 
 CURR_PWD=$(pwd)
 
-# Para cada directorio de conjunto de datos
+# Para cada directorio de conjunto de datos hacer 10 corridas de 10-CV
 for i in `seq 1 7`; do
-	# RandomForest con 10 corridas de 10-CV
 	for CV_SEED in `seq 1 10`; do
-		mkdir -p $CURR_PWD/results/$i
-		OUTPUT_FILE=$CURR_PWD/results/$i/RandomForest_$CV_SEED.txt
-		java weka.classifiers.trees.RandomForest -P 100 -I 100 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1 -s $CV_SEED -t $CURR_PWD/$i/data_filled_r.arff > $OUTPUT_FILE
+		for DATASET in data data_filled_r data_filled_weka; do
+			mkdir -p $CURR_PWD/results/$i
+			OUTPUT_DIR=$CURR_PWD/results/$i
+			# RandomForest
+			OUTPUT_FILE=$OUTPUT_DIR/RandomForest_$DATASET_$CV_SEED.txt
+			java weka.classifiers.trees.RandomForest -P 100 -I 100 -num-slots 1 -K 0 -M 1.0 -V 0.001 -S 1 -s $CV_SEED -t $CURR_PWD/$i/$DATASET.arff > $OUTPUT_FILE
+
+			# RandomTree
+			OUTPUT_FILE=$OUTPUT_DIR/RandomTree_$DATASET_$CV_SEED.txt
+			java weka.classifiers.trees.RandomTree -K 0 -M 1.0 -V 0.001 -S 1 -s $CV_SEED -t $CURR_PWD/$i/$DATASET.arff > $OUTPUT_FILE
+
+			# Otros...
+		done
 	done
 done
