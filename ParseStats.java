2735ad9008f918dbdef8d8aa670e3ac03ffda6bb
Guillermo Osorio <gaompy@gmail.com>
2018-12-12 06:04:32 -0300

    Generación de tablas estadísticas de rendimiento por algoritmo clasificador

--- ParseStats.java
+++ ParseStats.java
@@ -27,6 +27,10 @@ import java.util.Hashtable;
 import java.util.Arrays;
 import java.util.stream.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 public class ParseStats {
     public ParseStats() {
         super();
@@ -62,13 +66,20 @@ public class ParseStats {
         return Arrays.stream(array).mapToDouble(Double::doubleValue).sum() / array.length;
     }
 
-    String[] datasets = new String[]{
-        "/home/gosorio/Projects/qoe_datasets/1/data_filled_r.arff",
-        "/home/gosorio/Projects/qoe_datasets/1/data_filled_weka.arff"
+    public static String[] datasets = new String[]{
+        "./4/data_filled_r.arff",
+        "./4/data_filled_weka.arff"
+    };
+
+    public static String[] results = new String[]{
+        "./results/4/stats_data_filled_r",
+        "./results/4/stats_data_filled_weka"
     };
 
     public static void main(String[] args) throws Exception {
         Evaluation eval;
+
+        int classIndex = 20;
 
         Classifier[] algoritmos = new Classifier[]{
             new MultilayerPerceptron(),
@@ -94,29 +105,155 @@ public class ParseStats {
         // Para cada dataset
         for (int j=0; j < datasets.length; j++){
             ParseStats parseStats = new ParseStats();
-            
             // Conjunto de datos a analizar
             parseStats.setTraining(datasets[j]);
-
             // Para cada clasificador
             for (int i=0; i < algoritmos.length; i++){
-                // Aleatoriedad de los subconjuntos para correr el 10-CV
-                for (parseStats.key_fold = 1; parseStats.key_fold < 11; parseStats.key_fold++){
-                    // Selección de algoritmo clasificador
-                    parseStats.m_Classifier = algoritmos[i];
-                    // Corremos el algoritmo
-                    eval = parseStats.execute();
-                    // Sección de promedios para cada corrida de 10-CV
+                try {
+                    PrintWriter pw = new PrintWriter(new File(results[j] + "_algoritmo" + i + ".csv"));
+                    // Campos del archivo CSV
+                    pw.println(
+                        "areaUnderPRC,areaUnderROC,avgCost,correct,coverageOfTestCasesByPredictedRegions," +
+                        "errorRate,falseNegativeRate,falsePositiveRate,fMeasure,incorrect,kappa,KBInformation," +
+                        "KBMeanInformation,meanAbsoluteError,numFalseNegatives,numFalsePositives,numInstances," +
+                        "numTrueNegatives,numTruePositives,pctCorrect,pctIncorrect,pctUnclassified,precision," +
+                        "recall,relativeAbsoluteError,rootMeanSquaredError,totalCost,trueNegativeRate," +
+                        "truePositiveRate,unclassified,weightedAreaUnderPRC,weightedAreaUnderROC," +
+                        "weightedFalseNegativeRate,weightedFalsePositiveRate,weightedFMeasure," +
+                        "weightedRecall,weightedTrueNegativeRate,weightedTruePositiveRate,key_fold"
+                    );
+    
+                    // Aleatoriedad de los subconjuntos para correr el 10-CV
+                    for (parseStats.key_fold = 1; parseStats.key_fold < 11; parseStats.key_fold++){
+                        // Crear archivos de salida
+                        
+                            // Selección de algoritmo clasificador
+                            parseStats.m_Classifier = algoritmos[i];
+                            // Corremos el algoritmo
+                            eval = parseStats.execute();
+                            // Sección de recolección de métricas para cada corrida de 10-CV
+                            StringBuilder sb = new StringBuilder();
+                            try {
+                                sb.append(eval.areaUnderPRC(classIndex));
+                            } catch (NullPointerException e) {
+                                sb.append(Double.NaN);
+                                e.printStackTrace();
+                            };
+                            sb.append(',');
+                            try {
+                                sb.append(eval.areaUnderROC(classIndex));
+                            } catch (NullPointerException e) {
+                                sb.append(Double.NaN);
+                                e.printStackTrace();
+                            };
+                            sb.append(',');
+                            sb.append(eval.avgCost());
+                            sb.append(',');
+                            sb.append(eval.correct());
+                            sb.append(',');
+                            sb.append(eval.coverageOfTestCasesByPredictedRegions());
+                            sb.append(',');
+                            sb.append(eval.errorRate());
+                            sb.append(',');
+                            sb.append(eval.falseNegativeRate(classIndex));
+                            sb.append(',');
+                            sb.append(eval.falsePositiveRate(classIndex));
+                            sb.append(',');
+                            try {
+                                sb.append(eval.fMeasure(classIndex));
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                sb.append(Double.NaN);
+                                e.printStackTrace();
+                            };
+                            sb.append(',');
+                            sb.append(eval.incorrect());
+                            sb.append(',');
+                            sb.append(eval.kappa());
+                            sb.append(',');
+                            sb.append(eval.KBInformation());
+                            sb.append(',');
+                            sb.append(eval.KBMeanInformation());
+                            sb.append(',');
+                            sb.append(eval.meanAbsoluteError());
+                            sb.append(',');
+                            sb.append(eval.numFalseNegatives(classIndex));
+                            sb.append(',');
+                            sb.append(eval.numFalsePositives(classIndex));
+                            sb.append(',');
+                            sb.append(eval.numInstances());
+                            sb.append(',');
+                            sb.append(eval.numTrueNegatives(classIndex));
+                            sb.append(',');
+                            sb.append(eval.numTruePositives(classIndex));
+                            sb.append(',');
+                            sb.append(eval.pctCorrect());
+                            sb.append(',');
+                            sb.append(eval.pctIncorrect());
+                            sb.append(',');
+                            sb.append(eval.pctUnclassified());
+                            sb.append(',');
+                            try {
+                                sb.append(eval.precision(classIndex));
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                sb.append(Double.NaN);
+                                e.printStackTrace();
+                            };
+                            sb.append(',');
+                            try {
+                                sb.append(eval.recall(classIndex));
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                sb.append(Double.NaN);
+                                e.printStackTrace();
+                            };
+                            sb.append(',');
+                            sb.append(eval.relativeAbsoluteError());
+                            sb.append(',');
+                            sb.append(eval.rootMeanSquaredError());
+                            sb.append(',');
+                            sb.append(eval.totalCost());
+                            sb.append(',');
+                            try {
+                                sb.append(eval.trueNegativeRate(classIndex));
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                sb.append(Double.NaN);
+                                e.printStackTrace();
+                            };
+                            sb.append(',');
+                            try {
+                                sb.append(eval.truePositiveRate(classIndex));
+                            } catch (ArrayIndexOutOfBoundsException e) {
+                                sb.append(Double.NaN);
+                                e.printStackTrace();
+                            };
+                            sb.append(',');
+                            sb.append(eval.unclassified());
+                            sb.append(',');
+                            sb.append(eval.weightedAreaUnderPRC());
+                            sb.append(',');
+                            sb.append(eval.weightedAreaUnderROC());
+                            sb.append(',');
+                            sb.append(eval.weightedFalseNegativeRate());
+                            sb.append(',');
+                            sb.append(eval.weightedFalsePositiveRate());
+                            sb.append(',');
+                            sb.append(eval.weightedFMeasure());
+                            sb.append(',');
+                            sb.append(eval.weightedRecall());
+                            sb.append(',');
+                            sb.append(eval.weightedTrueNegativeRate());
+                            sb.append(',');
+                            sb.append(eval.weightedTruePositiveRate());
+                            sb.append(',');
+                            sb.append(parseStats.key_fold);
+                            pw.println(sb.toString());
+                            pw.flush();
+                            System.out.println(sb.toString());
+                    }
+                    pw.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
                 }
             }
         }
     }
 }
-
-/**
-// Test
-                    Double[] array = new Double[]{0.559322033898305,0.4915254237288136,0.5084745762711864};
-                    System.out.println("Promedio de array con stream: " + promediarArray(array));
-                    
-                    System.out.println(parseStats.toString());
- */
